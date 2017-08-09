9ba9d4aedc1b54f15981fc2daff26f0482e7f49f
Android-罗志敏 <54543534@domain.com>
2017-08-09 14:42:57 +0800

    直播栏界面调整11111

--- GXCJZBS/src/com/jyh/gxcjzbs/fragment/fragment_zb.java
+++ GXCJZBS/src/com/jyh/gxcjzbs/fragment/fragment_zb.java
@@ -223,7 +223,7 @@ public class fragment_zb extends Fragment implements OnClickListener {
         rollDotViewpager=new RollDotViewPager(getContext());
         LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.MATCH_PARENT,
-                SystemUtil.dp2px(getContext(), 110));
+                SystemUtil.dp2px(getContext(), 105));
 
         rollDotViewpager.setLayoutParams(lp);
 
